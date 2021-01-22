bc078908d9d167c116e563d9366fbfe6a2465311
Adam Marsala <adammarsala@hotmail.com>
2021-01-22 17:09:53 -0500

    Axes set up in CSS

--- BarChart.js
+++ BarChart.js
@@ -1,6 +1,24 @@
 $(function () {
-  $( "a" ).on("click", function( event ) {
-  alert( "As you can see, the link no longer took you to jquery.com" );
-  event.preventDefault();
-  });
+
+  const data = [1, 2, 3, 4, 5];
+
+  let options = {
+    width: 500,
+    height: 500
+  };
+
+  const drawBarChart = (data, options, element) => {
+
+  }
+
+// The data parameter will be the data the chart should work from
+// Start with just an Array of numbers e.g. [1, 2, 3, 4, 5]
+
+// The options parameter should be an object which has options for the chart.
+// e.g. width and height of the bar chart
+
+// The element parameter should be a DOM element or jQuery element that the chart will get rendered into.
+
+
+
 });
