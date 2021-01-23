77a2dd61ac1c340734ac5bf94578fccdccab7e12
Adam Marsala <adammarsala@hotmail.com>
2021-01-22 21:52:15 -0500

    The Chart has Bars

--- BarChart.js
+++ BarChart.js
@@ -1,14 +1,35 @@
 $(function () {
 
   const data = [1, 2, 3, 4, 5];
+  const $chart = $("#chart");
 
   let options = {
-    width: 500,
-    height: 500
+    width: $chart.width("30vw"),
+    height: $chart.height("30vw")
   };
 
-  const drawBarChart = (data, options, element) => {
+  // create HTML Elements for options to customize (buttons, tick boxes, inputs) and link to options
 
+  const drawBarChart = (data, options, element) => {
+    for (let i = 0; i < data.length; i++) {
+      let bar = document.createElement('div');
+      bar.setAttribute("class", "bar");
+      bar.innerHTML = data[i];
+      //bar.style.height = data[i]*10 + "px"; // Cannot change height of inline elements!
+      element.append(bar);
+    }
+    let $bars = $(".bar"); // this is an array of all bar divs
+    $bars.each(function(i) {
+      this.style.display = "inline";
+      this.style.background = "blue";
+      this.style.padding = "0 10px";
+      this.style.margin = "0 5px"; // TopandBottom LeftandRight
+      //this.style.height = data[i]*10 + "%";
+      this.style.paddingTop = data[i]*10 + "%";
+     });
+    // height of each bar is dependent on data values
+    // add transitions for height
+    // add/change css properties dynamically based on options/event listeners
   }
 
 // The data parameter will be the data the chart should work from
@@ -19,6 +40,6 @@ $(function () {
 
 // The element parameter should be a DOM element or jQuery element that the chart will get rendered into.
 
-
+  drawBarChart(data, options, $chart); // "chart" is the div element with class "axes" and id "chart"
 
 });
