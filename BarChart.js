45b45b093a0c9950c95469b71dc4ca273af06224
Adam Marsala <adammarsala@hotmail.com>
2021-01-26 16:04:06 -0500

    Functional BarChart and Non-Functional options

--- BarChart.js
+++ BarChart.js
@@ -1,33 +1,54 @@
 $(function () {
 
-  const data = [1, 2, 3, 4, 5];
+  //const data = [10, 5, 35, 42, 21, 50, 10, 8, 66];
+  const data = [25, 100, 66, 79, 42, 30, 25]; // RIGHT NOW THE MAX Number of values it can handle is 7 before the $chart width (40vw) is too narrow...
   const $chart = $("#chart");
 
   let options = {
-    width: $chart.width("30vw"),
-    height: $chart.height("30vw")
+    width: $chart.width("40vw"), // change this to a "getter" method from input?
+    height: $chart.height("40vw") // change this to a "getter" method from input?
   };
 
   // create HTML Elements for options to customize (buttons, tick boxes, inputs) and link to options
 
   const drawBarChart = (data, options, element) => {
+    let gridTemplateCols = "";
+    let highestValue = Math.max(...data);
+    let gridTemplateRows = "repeat(" + highestValue + ", 1fr)";
+
+    /*const setBarHeight = (i) => { // Returns integer for row number to start at from top (if top is row 1)
+      let rowStart = 1 + highestValue - data[i];
+      console.log(rowStart);
+      return rowStart;
+    }*/
+
     for (let i = 0; i < data.length; i++) {
       let bar = document.createElement('div');
       bar.setAttribute("class", "bar");
       bar.innerHTML = data[i];
-      //bar.style.height = data[i]*10 + "px"; // Cannot change height of inline elements!
+      bar.style["grid-column-start"] = i + 1;
+      bar.style["grid-row-start"] = 1 + highestValue - data[i];
+      bar.style["grid-row-end"] = -1;
       element.append(bar);
+      gridTemplateCols += "1fr ";
     }
+
+    console.log(gridTemplateRows);
+
+    $chart.css("grid-template-columns", gridTemplateCols);
+    $chart.css("grid-template-rows", gridTemplateRows);
+
     let $bars = $(".bar"); // this is an array of all bar divs
+
     $bars.each(function(i) {
-      this.style.display = "inline";
-      this.style.background = "blue";
-      this.style.padding = "0 10px";
-      this.style.margin = "0 5px"; // TopandBottom LeftandRight
+      // this.style.display = "inline";
+      // this.style.background = "blue";
+      // this.style.padding = "0 10px";
+      //this.style.margin = "0 5px"; // TopandBottom LeftandRight
       //this.style.height = data[i]*10 + "%";
-      this.style.paddingTop = data[i]*10 + "%";
+      //this.style.paddingTop = data[i]*10 + "%";
      });
-    // height of each bar is dependent on data values
+
     // add transitions for height
     // add/change css properties dynamically based on options/event listeners
   }
